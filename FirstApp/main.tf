134d0fc8b22dc64af84602c8c8e3c12079ef47d2
Govind V <govindv108@gmail.com>
2023-11-20 08:48:19 -0500

    firstcommit

---
+++ FirstApp/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "~> 1.0" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "first_ec2_instance" {
+  ami = "ami-0230bd60aa48260c6"
+  instance_type = "t2.micro"
+}
